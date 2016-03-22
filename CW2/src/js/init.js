195e0b808c246e21e2282ff5ac726b4488e9042b
Jacob Holland <Jacob Holland>
2016-03-22 20:25:26 +0000

    Home page almost done

--- CW2/src/js/init.js
+++ CW2/src/js/init.js
@@ -1,8 +1,12 @@
-(function($){
-  $(function(){
+(function ($) {
+    $(function () {
 
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+        // Show sideNav
+        $('.button-collapse').sideNav('show');
+        // Hide sideNav
+        $('.button-collapse').sideNav('hide');
 
-  }); // end of document ready
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
