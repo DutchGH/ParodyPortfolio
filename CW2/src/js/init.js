20939cf8fda65d9b03f96e040dc2980fe657849d
Jacob Holland <Jacob Holland>
2016-05-05 00:23:23 +0100

    main layout of all websites done

--- CW2/src/js/init.js
+++ CW2/src/js/init.js
@@ -1,8 +1,11 @@
-(function($){
-  $(function(){
-
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
+(function ($) {
+  $(function () {
+      $('.button-collapse').sideNav();
+      $('.parallax').parallax();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$(document).ready(function() {
+$('input#input_text, textarea#textarea1').characterCounter();
+});
